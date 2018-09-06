43390e77411c73f7d6fda24042aaecb0ab57136e
lauraslopes <lauraslopes94@gmail.com>
2018-09-06 13:39:37 -0300

    começo

---
+++ iris-lamp-mask.py
@@ -0,0 +1,316 @@
+#!/usr/bin/python
+
+import cv2, os
+import cv2.cv as cv
+import cv as cv1
+import numpy as np
+from PIL import Image
+import math
+import pywt
+import matplotlib.pyplot as plt
+np.set_printoptions(threshold=np.nan)
+
+
+def get_iris_radius (img, circles):
+	circles = np.uint8(np.around(circles))
+
+
+	circles[0,0,2] += 30
+	circle_img1 = np.zeros(img.shape, dtype = 'uint8')
+	for i in circles[0,:]:
+		cv2.circle(circle_img1,(i[0],i[1]),i[2],255,1)	# draw the outer circle
+
+	rect_img = np.zeros(img.shape, dtype = 'uint8')
+	cv2.rectangle(rect_img, (0, int(circles[0,0,1] + circles[0,0,2]*0.7)), (1000, int(circles[0,0,1] - circles[0,0,2]*0.7)), 255, -1)
+
+	circle_img2 = cv2.bitwise_and(circle_img1, rect_img)
+	circle_img3 = cv2.bitwise_and(circle_img2, img)
+
+	circle_points = np.extract (circle_img3, img)
+	mean_circle_prev = np.mean (circle_points)
+
+	#print '\n\n'
+	for x in xrange(1,40):
+		circles[0,0,2] += 1
+		circle_img1 = np.zeros(img.shape, dtype='uint8')
+		for i in circles[0,:]:
+			cv2.circle(circle_img1,(i[0],i[1]),i[2],255,1)	# draw the outer circle
+
+		rect_img = np.zeros(img.shape, dtype = 'uint8')
+		cv2.rectangle(rect_img, (0, int(circles[0,0,1] + circles[0,0,2]*0.7)), (1000, int(circles[0,0,1] - circles[0,0,2]*0.7)), 255, -1)
+
+		circle_img2 = cv2.bitwise_and(circle_img1, rect_img)
+		circle_img3 = cv2.bitwise_and(circle_img2, img)
+
+		circle_points = np.extract (circle_img3, img)
+		mean_circle = np.mean (circle_points)
+
+		diff = mean_circle - mean_circle_prev
+		if (diff > 6):
+			#print diff
+			return circles[0,0,2]
+
+		mean_circle_prev = mean_circle
+
+	return circles[0,0,2]
+
+
+def theta_transform (theta, M):
+    return ((2*math.pi*theta) / (M-1))
+
+def rho_transform(rho, rho_max, rho_min, R):
+    return ((rho * (rho_max - rho_min) / (R-1) ) + rho_min)
+
+def bilinear_interpolation(img, x, y):
+    #print img[int(y),int(x)]
+    x1 = int(math.floor(x))
+    x2 = int(math.ceil(x))
+    y1 = int(math.floor(y))
+    y2 = int(math.ceil(y))
+
+    if y1 >= 480:
+    	y1 = 479
+    if y2 >= 480:
+    	y2 = 479
+    if x1 >= 640:
+    	x1 = 639
+    if x2 >= 640:
+    	x2 = 639
+    if x >= 640:
+    	x = 639
+    if y >= 480:
+    	y = 479
+
+    if x1 == x2:
+        f_xy1 = img[y1,int(x)]
+        f_xy2 = img[y2,int(x)]
+    else:
+        f_xy1 = (x2 - x)/(x2 - x1) * img[y1,x1] + (x - x1)/(x2 - x1) * img[y1,x2]
+        f_xy2 = (x2 - x)/(x2 - x1) * img[y2,x1] + (x - x1)/(x2 - x1) * img[y2,x2]
+
+    if y1 == y2:
+        return f_xy1
+    else:
+        f_xy = (y2 - y)/(y2 - y1) * f_xy1 + (y - y1)/(y2 - y1) * f_xy2
+
+    return f_xy
+
+
+def normalize_img (img, pupil_radius, iris_radius, x_center, y_center):
+	normalized = np.zeros((300,70), dtype='uint8')
+	for i in xrange(0,300):
+	    for j in xrange(0,70):
+	        new_theta = theta_transform (theta=i, M=300)
+	        new_rho = rho_transform (rho=j, rho_min=pupil_radius, rho_max=iris_radius, R=70)
+
+	        x = new_rho * math.cos(new_theta) + x_center
+	        y = new_rho * math.sin(new_theta) + y_center
+
+	        normalized[i,j] = int(round(bilinear_interpolation (img, y, x)))
+
+	normalized = np.rot90(normalized, 1)
+	return normalized
+
+
+class Subject:
+	def __init__(self, path):
+		self.path = path
+		self.img_list = []
+
+
+	def read_images(self):
+		#reading subject's left eye
+		self.L_paths = [os.path.join(self.path + '/L', f) for f in os.listdir(self.path + '/L')]
+		self.L_paths.sort()
+
+		#reading subject's right eye
+		self.R_paths = [os.path.join(self.path + '/R', f) for f in os.listdir(self.path + '/R')]
+		self.R_paths.sort()
+
+
+	def remove_spots(self, original_image, path):
+		mask_path = './CASIA-IrisV4-Lamp-100-mask/' + path[20:]
+		mask_image = cv2.imread(mask_path, 0)
+		if mask_image is not None:
+			th, mask_image = cv2.threshold(mask_image, 100, 255, cv2.THRESH_BINARY)
+
+			mask = np.zeros((482, 642), np.uint8)
+			cv2.floodFill(mask_image, mask, (0,0), 255);
+
+			preprocessed = cv2.bitwise_and(mask_image, original_image)
+
+			th, binarized = cv2.threshold(preprocessed, 50, 255, cv2.THRESH_BINARY)
+			return binarized
+		else:
+			blurred_image = cv2.medianBlur(original_image, 5) #####
+
+			th, im_th = cv2.threshold(blurred_image, 40, 255, cv2.THRESH_BINARY_INV)
+
+			kernel = np.ones((3,1),np.uint8)
+			erosion1 = cv2.erode(im_th, kernel, iterations = 1)
+
+			kernel = np.ones((1,3),np.uint8)
+			erosion2 = cv2.erode(erosion1, kernel, iterations = 1)
+
+			mask = np.zeros((482, 642), np.uint8)
+			im_floodfill = erosion2.copy()
+			cv2.floodFill(im_floodfill, mask, (300,200), 255);
+
+			im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+			pupil_mask = erosion2 | im_floodfill_inv
+			pupil_mask = cv2.bitwise_not(pupil_mask)
+
+			preprocessed = cv2.bitwise_and(pupil_mask, original_image)
+
+			th, binarized = cv2.threshold(preprocessed, 45, 255, cv2.THRESH_BINARY)
+
+			'''
+			cv2.imshow('norm', original_image)
+			cv2.waitKey(0)
+
+			cv2.imshow('norm', im_th)
+			cv2.waitKey(0)
+
+			cv2.imshow('norm', im_floodfill)
+			cv2.waitKey(0)
+
+			cv2.imshow('norm', binarized)
+			cv2.waitKey(0)
+			'''
+
+			return binarized
+
+	def get_iris_features(self):
+		for path in [item for sublist in [self.L_paths,self.R_paths] for item in sublist]:
+
+			#
+			print 'computing features: ' + path
+			#
+
+			original_image = cv2.imread(path, 0)
+
+			binarized = self.remove_spots(original_image, path)
+
+			edges = cv2.Canny(binarized,100,200)
+			#circles = cv2.HoughCircles(image=cv2.GaussianBlur(mask_image, (5,5), 0),method=cv.CV_HOUGH_GRADIENT,dp=2,minDist=220,param1=500,param2=50,minRadius=20,maxRadius=70)
+			circles = cv2.HoughCircles(image=cv2.GaussianBlur(edges, (5,5), 0),method=cv.CV_HOUGH_GRADIENT,dp=2,minDist=220,param1=500,param2=50,minRadius=20,maxRadius=70)
+
+			if circles is None:
+				break
+
+			pupil_radius = circles[0,0,2]
+			equalized_img = cv2.equalizeHist(original_image)
+			iris_radius = get_iris_radius (equalized_img, circles)
+
+			normalized = normalize_img(original_image, pupil_radius, iris_radius, circles[0,0,1], circles[0,0,0])
+
+			wav = pywt.wavedec2(normalized, 'haar', level=4)
+			cA = wav[0]
+			(cH, cV, cD) = wav[1]
+
+			feature_template = (cH*3 + cV + cD)/5
+			
+			iris_code = (feature_template >= 0)  #binarizing image with threshold_value 0
+			label = path[26:-4]
+
+			self.img_list.append([label, iris_code.flatten()])
+
+
+			#cv2.imshow('norm', normalized)
+			#cv2.waitKey(10)
+
+
+
+
+def hamming_distance(array1, array2):
+	n = array1.shape[0]
+	xor_op = np.logical_xor(array1, array2).sum()
+	return xor_op / float(n)
+
+
+path = "CASIA-Iris-Lamp-100"
+
+subj_paths = [os.path.join(path, f) for f in os.listdir(path)]
+subj_paths.sort()
+
+subjects = []
+
+for subj_path in subj_paths:
+	new_subj = Subject(subj_path)
+	subjects.append(new_subj)
+
+for subject in subjects:
+	subject.read_images()
+	subject.get_iris_features()
+
+print 'computing metrics...'
+
+img_list_all = [img_list for subj in subjects for img_list in subj.img_list]
+
+image_num = len(img_list_all)
+distance_matrix = np.zeros((image_num, image_num), dtype=object)
+values = np.zeros((image_num, image_num), dtype=float)
+
+print 'getting distance_matrix'
+for i in xrange(0,image_num):
+	for j in xrange(0,image_num):
+		a = img_list_all[i]
+		b = img_list_all[j]
+		distance_matrix[i,j] = [hamming_distance(a[1], b[1]), a[0][:-2] == b[0][:-2], a[0] + ',' + b[0]]
+		values[i,j] = hamming_distance(a[1], b[1])
+
+upper_indices = np.triu_indices(image_num)
+distance_matrix = distance_matrix[upper_indices]
+distance_matrix = np.sort(distance_matrix)
+
+values = np.unique(values)
+values = np.append(values, 1.0)
+
+
+positives_total = float(0)
+negatives_total = float(0)
+
+print 'getting positives_total and negatives_total'
+for dist in distance_matrix:
+	if dist[1] == True:
+		positives_total += 1
+	else:
+		negatives_total += 1
+
+
+false_positives = []
+false_negatives = []
+
+print 'getting false_positives and false_negatives'
+for threshold in values:
+	fp = 0
+	fn = 0
+
+	for dist in distance_matrix:
+		if dist[0] <= threshold and dist[1] == False:
+			fp += 1
+		elif dist[0] > threshold and dist[1] == True:
+			fn += 1
+
+	fp = fp / float(negatives_total)
+	fn = fn / float(positives_total)
+	false_positives.append(fp)
+	false_negatives.append(fn)
+
+false_positives = np.array(false_positives)
+false_negatives = np.array(false_negatives)
+
+differences = np.absolute(false_positives - false_negatives)
+index_EER = np.argmin(differences)
+
+print 'false_negatives:'
+print false_negatives
+print 'false_positives:'
+print false_positives
+print 'EER: {},{}'.format(false_positives[index_EER], false_negatives[index_EER])
+
+plt.axis([-0.1, 1.1, -0.1, 1.1])
+plt.plot(false_positives, false_negatives, linestyle='--')
+plt.plot(false_positives[index_EER], false_negatives[index_EER], 'o', color='red')
+plt.show()
---
+++ iris_recognizer.py
@@ -0,0 +1,478 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+# Import the required modules
+import cv2, os
+import numpy as np
+from PIL import Image
+import math
+import pywt
+from skimage import feature
+from sklearn import svm
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+
+class Dataset:
+    def __init__(self, path):
+        self.path = path
+        self.images = []
+        self.labels = []
+        self.codes = []
+        self.iris = []
+
+    #funções comuns entre os datasets
+    def get_images_and_labels(self):
+
+        for d in os.listdir(self.path):
+            directory = os.path.join(self.path, d)
+            for sd in os.listdir(directory): #L ou R
+                subdirectory = os.path.join(directory,sd)
+                for f in os.listdir(subdirectory):
+                    if f.endswith('.jpg'): #é uma imagem
+                        image_path = os.path.join(subdirectory, f)
+                        image_pil = Image.open(image_path)
+                        # Convert the image format into numpy array
+                        image = np.array(image_pil, 'uint8')
+                        self.images.append(image)
+                        # Get the label of the image (de qual pessoa pertence)
+                        if subdirectory.endswith('L'):
+                            self.labels.append(int(os.path.split(image_path)[1].split("L")[0].replace("S", "")))
+                        else:
+                            self.labels.append(int(os.path.split(image_path)[1].split("R")[0].replace("S", "")))
+
+#criar mascaras
+def preprocess_lamp(image):
+
+    smoothed = cv2.medianBlur(image,5)
+    num, mask = cv2.threshold(smoothed, 35, 255, cv2.THRESH_BINARY_INV)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    mask = cv2.dilate(mask, kernel, iterations=5)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    mask = cv2.erode(mask, kernel, iterations=4)
+    # cv2.imshow("Preprocesdsdsdsdsing...", mask)
+    # cv2.waitKey(0)
+    #aumenta um pouco o círculo
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    mask = cv2.dilate(mask, kernel, iterations=9)
+    # cv2.imshow("Preprocesdsdsdsdsing...", mask)
+    # cv2.waitKey(0)
+
+    mask = cv2.Canny(mask,100,200)
+
+    circles = cv2.HoughCircles(mask,method=cv2.HOUGH_GRADIENT,dp=2,minDist=220,param1=500,param2=50,minRadius=20,maxRadius=70)
+    #dependendo da versão do python, descomentar a linha abaixo e comentar a anterior
+    # circles = cv2.HoughCircles(mask,method=cv2.cv.CV_HOUGH_GRADIENT,dp=2,minDist=220,param1=500,param2=50,minRadius=20,maxRadius=70)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            centerx = i[0]
+            centery = i[1]
+            radius = i[2]
+            # cv2.circle(smoothed, (centerx,centery), radius, (255, 0, 255), 1)
+            # cv2.imshow("Image", smoothed)
+            # cv2.waitKey(50)
+
+        circles = circles[0][0]
+
+    return circles
+
+def preprocess_interval(image):
+
+    smoothed = cv2.medianBlur(image,5)
+    # cv2.imshow('smoothed', smoothed)
+    num, mask = cv2.threshold(smoothed, 100, 255, cv2.THRESH_BINARY_INV)
+    # cv2.imshow('mask', mask)
+    # cv2.waitKey()
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    mask = cv2.dilate(mask, kernel, iterations=7)
+    # cv2.imshow('dilate', mask)
+    # cv2.waitKey()
+
+    mask = cv2.erode(mask, kernel, iterations=4)
+    # cv2.imshow("erode", mask)
+    # cv2.waitKey(0)
+
+    mask = cv2.Canny(mask,100,200)
+
+    circles = cv2.HoughCircles(mask,method=cv2.HOUGH_GRADIENT,dp=2,minDist=220,param1=500,param2=50,minRadius=20,maxRadius=70)
+    #dependendo da versão do python, descomentar a linha abaixo e comentar a anterior
+    #circles = cv2.HoughCircles(mask,method=cv2.cv.CV_HOUGH_GRADIENT,dp=2,minDist=220,param1=500,param2=50,minRadius=20,maxRadius=70)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            centerx = i[0]
+            centery = i[1]
+            radius = i[2]
+            # cv2.circle(smoothed, (centerx,centery), radius, (255, 0, 255), 1)
+            # cv2.imshow("Image", smoothed)
+            # cv2.waitKey(50)
+
+        circles = circles[0][0]
+
+    return circles
+
+def get_iris (image, circle):
+    centerx, centery, radius = circle
+
+    #mascara com o raio da pupila
+    pupil = np.zeros(image.shape, dtype = 'uint8')
+    cv2.circle(pupil, (centerx,centery), radius, 255, 1)    # draw the outer circle
+    # cv2.imshow("Preprocesdsdsdsdsing...", pupil)
+    # cv2.waitKey(0)
+
+    image_pupil = cv2.bitwise_and(image, image, mask=pupil)
+    # cv2.imshow("Preprocesdsdsdsdsing...", img1)
+    # cv2.waitKey(0)
+    masks = np.zeros(image.shape, dtype='uint8')
+    cv2.circle(masks, (centerx,centery), radius + 15, 255, 3) #aumenta raio em 2 pixels
+    image_iris = cv2.bitwise_and(image, image, mask=masks)
+    mean_ant = np.mean(image_iris)
+
+    max_mean = -1
+    max_radius = 0
+    for r in range((radius+16), 120):
+        masks = np.zeros(image.shape, dtype='uint8')
+        cv2.circle(masks, (centerx,centery), r, 255, 3) #aumenta raio em 1 pixel
+        image_iris = cv2.bitwise_and(image, image, mask=masks)
+        mean = np.mean(image_iris)
+        outraimg = image.copy()
+        # cv2.circle(outraimg, (centerx,centery), r, 255, 2)
+        # cv2.imshow("...oi", outraimg)
+        # cv2.waitKey()
+        diff = mean - mean_ant
+        mean_ant = mean
+        if diff > max_mean:
+            # img = image.copy()
+            # cv2.circle(img, (centerx,centery), r, 255, 2)
+            # cv2.imshow("...", img)
+            # cv2.waitKey()
+            max_mean = diff
+            max_radius = r
+
+    # cv2.circle(image,(centerx,centery), max_radius, 255, 2)
+    # cv2.imshow("Preprocesdsdsdsdsing...", image)
+    # cv2.waitKey()
+    return max_radius
+
+def normalize_iris(image, pupil, radius_iris):
+
+    total_grades = 2*math.pi
+    num_pixels = radius_iris - pupil[2] #raio da iris menos raio pupila
+    interval = np.linspace(0, total_grades, num=360)
+    polar = np.zeros((num_pixels, len(interval)), dtype='uint8') #matriz para a iris normalizada
+    for pixel in xrange(0, num_pixels):
+        offset = 0
+        ant = 0
+        for i in interval:
+            grade = i
+            x = int((pupil[2] + pixel)*math.cos(grade) + pupil[0]) # centerx
+            y = int((pupil[2] + pixel)*math.sin(grade) + pupil[1]) # centery
+            # print pixel, int(grade*total_grades+offset), offset
+            if (int(grade*total_grades+offset) == (ant+2)):
+                offset-=1
+            if y < image.shape[0] and x < image.shape[1]:
+                polar[pixel, int(grade*total_grades+offset)] = image[y,x] #pq a imagem é deitada
+            else:
+                polar[pixel, int(grade*total_grades+offset)] = 0 #iris fora da imagem
+            ant = int(grade*total_grades+offset)
+            offset+=1
+            if (grade*total_grades+offset) >= 360:
+                break
+
+    # cv2.imshow("Preprocesdsdsdsdsing...", polar)
+    # cv2.waitKey()
+    # cv2.imwrite("oioi.png", polar)
+    image = polar
+    return image
+
+def hamming_distance(code_query, code_database):
+    nm = code_query.shape[0] * code_query.shape[1]
+    xor = np.logical_xor(code_query, code_database).sum()
+    return xor / float(nm)
+
+np.set_printoptions(threshold=np.nan)
+# Path to the Lamp-100 Dataset
+path_lamp = './CASIA-Iris-Lamp-100'
+casia_lamp = Dataset(path_lamp)
+print "Lendo dataset CASIA Lamp"
+casia_lamp.get_images_and_labels()
+print (str(len(casia_lamp.images))+" imagens")
+c = centerx = centery = radius_iris = radius_pupil = 0
+for image in casia_lamp.images:
+    print "Pré-processando a imagem "+str(c+1)+" e detectando a pupila"
+    pupil = preprocess_lamp(image)
+    if pupil is not None: #achou a pupila
+        print "Detectando a iris"
+        radius_iris = get_iris(image, pupil)
+        print "Normalizando a iris"
+        image_normalized = normalize_iris(image, pupil, radius_iris)
+        casia_lamp.iris.append(image_normalized)
+        #binariza a imagem com Haar Wavelet
+        print "Realizando Haar Wavelet"
+        coefficients = pywt.wavedec2(image_normalized, 'haar', level=4)
+        cA = coefficients[0] #Approximation
+        (cH, cV, cD) = coefficients[1] #horizontal detail, vertical detail and diagonal detail
+        image_binar = np.zeros((cA.shape[0], cA.shape[1]), dtype='uint8')
+        for i in xrange(cA.shape[0]):
+            for j in xrange(cA.shape[1]):
+                if ((cH[i,j]>=0) and (cV[i,j]>=0) and (cD[i,j]>=0)):
+                    image_binar[i,j] = 1
+                else:
+                    image_binar[i,j] = 0
+        image_binar = np.resize(image_binar, (5,23)) #deixar todos do mesmo tamanho
+        # img = image_binar.copy()
+        # for i in xrange(img.shape[0]):
+        #     for j in xrange(img.shape[1]):
+        #         if img[i,j] == 1:
+        #             img[i,j] = 255
+        # cv2.imshow('binar',img)
+        # cv2.waitKey()
+        casia_lamp.codes.append(image_binar) #salvo na ordem das labels tbm
+
+    else: #não achou a pupila, remove a imagem
+        np.delete(casia_lamp.images, i, 0)
+        c-=1
+    c+=1
+
+if (len(casia_lamp.images)%2 != 0): #se não for número par de imagens
+    np.delete(casia_lamp.images, len(casia_lamp.images)-1, 0)
+
+print "Hamming"
+#percorrer como uma matriz de iris codes triangular (para n repetir as distancias)
+distance = []
+true_or_false = []
+for i in xrange(0, len(casia_lamp.codes)): #3954 imagens e códigos
+    code_query = casia_lamp.codes[i]
+    for j in xrange(i+1, len(casia_lamp.codes)):
+        code_database = casia_lamp.codes[j]
+        distance.append(hamming_distance(code_query, code_database))
+        true_or_false.append((casia_lamp.labels[i] == casia_lamp.labels[j]))
+
+limits = np.sort(distance)
+limits = np.unique(limits)
+positives_total = 0
+negatives_total = 0
+for aux in true_or_false:
+    if aux == True: #mesma label
+        positives_total += 1
+    else:
+        negatives_total += 1
+
+false_positives = []
+false_negatives = []
+print "Detectando falsos positivos e falsos negativos"
+for limit in limits:
+    fp = 0
+    fn = 0
+    for j in xrange(len(distance)):
+        database = distance[j]
+        if database <= limit and true_or_false[j] == False:
+            fp += 1
+        elif database > limit and true_or_false[j] == True:
+            fn += 1
+
+    fp = fp / float(negatives_total)
+    fn = fn / float(positives_total)
+    false_positives.append(fp)
+    false_negatives.append(fn)
+
+print "Plotando gráficos FARxFRR"
+false_positives = np.array(false_positives)
+false_negatives = np.array(false_negatives)
+index_EER = np.argmin(np.absolute(false_positives - false_negatives)) #indice para a diferença mínima entre falso positivos e falso negativos
+plt.axis([0, 1.0, 0, 1.0])
+plt.title('FAR x FRR graph')
+plt.xlabel('False Positives')
+plt.ylabel('False Negatives')
+plt.plot(false_positives, false_negatives, linestyle='--', color='black')
+plt.plot(false_positives[index_EER], false_negatives[index_EER], 'o', color='green', label='EER')
+plt.legend()
+plt.savefig('DETcurve.png')
+
+lbps = []
+major = 0
+for img in casia_lamp.iris:
+    if img.shape[0] > major:
+        major = img.shape[0]
+
+c = 0
+for img in casia_lamp.iris:
+    img = np.resize(img, (major,img.shape[1]))
+    print ("LBP da imagem "+str(c+1))
+    lbps.append(feature.local_binary_pattern(img, img.shape[1], img.shape[0], method="uniform"))
+    c+=1
+
+#dividir imagens de treino e teste 10 vezes
+kfold = KFold(n_splits=10, shuffle=True)
+accuracy = 0
+for train_index, test_index in kfold.split(lbps):
+    data_train = []
+    data_test = []
+    label_train = []
+    label_test = []
+    # print("TRAIN:", train_index, "TEST:", test_index)
+    #labels referenciada pelo indice da imagem
+    for train in train_index:
+        data_train.append(lbps[train])
+        label_train.append(casia_lamp.labels[train])
+
+    print "Treinando SVM"
+    model = svm.SVC(kernel='linear', C = 1.0)
+    data_train = np.reshape(data_train, (len(data_train), data_train[0].shape[0]*data_train[0].shape[1]))
+    model.fit(data_train, label_train)
+
+    for test in test_index:
+        data_test.append(lbps[test])
+        label_test.append(casia_lamp.labels[test])
+
+    data_test = np.reshape(data_test, (len(data_test), data_test[0].shape[0]*data_test[0].shape[1]))
+    accuracy += model.score(data_test, label_test)
+
+print ("Mean accuracy for CASIA Lamp: "+str(accuracy/10))
+
+
+
+
+
+
+### CASIA INTERVAL ###
+path_interval = './CASIA-IrisV4-Interval'
+casia_interval = Dataset(path_interval)
+print "\n\nLendo dataset CASIA Interval"
+casia_interval.get_images_and_labels()
+print (str(len(casia_interval.images))+" imagens")
+c = centerx = centery = radius_iris = radius_pupil = 0
+for image in casia_interval.images:
+    print "Pré-processando a imagem "+str(c+1)+" e detectando a pupila"
+    pupil = preprocess_interval(image)
+    if pupil is not None: #achou a pupila
+        print "Detectando a iris"
+        radius_iris = get_iris(image, pupil)
+        print "Normalizando a iris"
+        image_normalized = normalize_iris(image, pupil, radius_iris)
+        casia_interval.iris.append(image_normalized)
+        #binariza a imagem com Haar Wavelet
+        print "Realizando Haar Wavelet"
+        coefficients = pywt.wavedec2(image_normalized, 'haar', level=4)
+        cA = coefficients[0] #Approximation
+        (cH, cV, cD) = coefficients[1] #horizontal detail, vertical detail and diagonal detail
+        image_binar = np.zeros((cA.shape[0], cA.shape[1]), dtype='uint8')
+        for i in xrange(cA.shape[0]):
+            for j in xrange(cA.shape[1]):
+                if ((cH[i,j]>=0) and (cV[i,j]>=0) and (cD[i,j]>=0)):
+                    image_binar[i,j] = 1
+                else:
+                    image_binar[i,j] = 0
+        image_binar = np.resize(image_binar, (5,23)) #deixar todos do mesmo tamanho
+        # img = image_binar.copy()
+        # for i in xrange(img.shape[0]):
+        #     for j in xrange(img.shape[1]):
+        #         if img[i,j] == 1:
+        #             img[i,j] = 255
+        # cv2.imshow('binar',img)
+        # cv2.waitKey()
+        casia_interval.codes.append(image_binar) #salvo na ordem das labels tbm
+
+    else: #não achou a pupila, remove a imagem
+        np.delete(casia_interval.images, i, 0)
+        c-=1
+    c+=1
+
+if (len(casia_interval.images)%2 != 0): #se não for número par de imagens
+    np.delete(casia_interval.images, len(casia_interval.images)-1, 0)
+
+print "Hamming"
+#percorrer como uma matriz de iris codes triangular (para n repetir as distancias)
+distance = []
+true_or_false = []
+for i in xrange(0, len(casia_interval.codes)): #3954 imagens e códigos
+    code_query = casia_interval.codes[i]
+    for j in xrange(i+1, len(casia_interval.codes)):
+        code_database = casia_interval.codes[j]
+        distance.append(hamming_distance(code_query, code_database))
+        true_or_false.append((casia_interval.labels[i] == casia_interval.labels[j]))
+
+limits = np.sort(distance)
+limits = np.unique(limits)
+positives_total = 0
+negatives_total = 0
+for aux in true_or_false:
+    if aux == True: #mesma label
+        positives_total += 1
+    else:
+        negatives_total += 1
+
+false_positives = []
+false_negatives = []
+print "Detectando falsos positivos e falsos negativos"
+for limit in limits:
+    fp = 0
+    fn = 0
+    for j in xrange(len(distance)):
+        database = distance[j]
+        if database <= limit and true_or_false[j] == False:
+            fp += 1
+        elif database > limit and true_or_false[j] == True:
+            fn += 1
+
+    fp = fp / float(negatives_total)
+    fn = fn / float(positives_total)
+    false_positives.append(fp)
+    false_negatives.append(fn)
+
+print "Plotando gráficos FARxFRR"
+false_positives = np.array(false_positives)
+false_negatives = np.array(false_negatives)
+index_EER = np.argmin(np.absolute(false_positives - false_negatives)) #indice para a diferença mínima entre falso positivos e falso negativos
+plt.axis([0, 1.0, 0, 1.0])
+plt.title('FAR x FRR graph')
+plt.xlabel('False Positives')
+plt.ylabel('False Negatives')
+plt.plot(false_positives, false_negatives, linestyle='--', color='black')
+plt.plot(false_positives[index_EER], false_negatives[index_EER], 'o', color='green', label='EER')
+plt.legend()
+plt.savefig('DETcurve.png')
+
+lbps = []
+major = 0
+for img in casia_interval.iris:
+    if img.shape[0] > major:
+        major = img.shape[0]
+
+c = 0
+for img in casia_interval.iris:
+    img = np.resize(img, (major,img.shape[1]))
+    print ("LBP da imagem "+str(c+1))
+    lbps.append(feature.local_binary_pattern(img, img.shape[1], img.shape[0], method="uniform"))
+    c+=1
+
+#dividir imagens de treino e teste 10 vezes
+kfold = KFold(n_splits=10, shuffle=True)
+accuracy = 0
+for train_index, test_index in kfold.split(lbps):
+    data_train = []
+    data_test = []
+    label_train = []
+    label_test = []
+    # print("TRAIN:", train_index, "TEST:", test_index)
+    #labels referenciada pelo indice da imagem
+    for train in train_index:
+        data_train.append(lbps[train])
+        label_train.append(casia_interval.labels[train])
+
+    print "Treinando SVM"
+    model = svm.SVC(kernel='linear', C = 1.0)
+    data_train = np.reshape(data_train, (len(data_train), data_train[0].shape[0]*data_train[0].shape[1]))
+    model.fit(data_train, label_train)
+
+    for test in test_index:
+        data_test.append(lbps[test])
+        label_test.append(casia_interval.labels[test])
+
+    data_test = np.reshape(data_test, (len(data_test), data_test[0].shape[0]*data_test[0].shape[1]))
+    accuracy += model.score(data_test, label_test)
+
+print ("Mean accuracy for CASIA Interval: "+str(accuracy/10))
